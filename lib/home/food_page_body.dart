34115da3b29eb5b47e11e1fc81ce9be89398776d
Matheus Fidelis <matheusfidelis@yahoo.com>
2022-03-17 15:28:00 -0300

    Updated Food Page with Widgets

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,3 +1,6 @@
+import 'package:e_commerce_app/utils/colors.dart';
+import 'package:e_commerce_app/widgets/big_text.dart';
+import 'package:e_commerce_app/widgets/small_text.dart';
 import 'package:flutter/material.dart';
 
 class FoodPageBody extends StatefulWidget {
@@ -8,8 +11,86 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  //aqui dizemos se o proximo slide deve ser visivel
+  PageController pageController = PageController(viewportFraction: 0.85);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.redAccent,
+      height: 320,
+      child: PageView.builder(
+        controller: pageController,
+        itemCount: 5,
+        itemBuilder: ((context, position) {
+          return _buildPageItem(position);
+        }),
+      ),
+    );
+  }
+
+  Widget _buildPageItem(int index) {
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(
+              left: 5, right: 5), //faz o espaço em branco entre as imagens
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(
+                "assets/image/food01.jpg",
+              ),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 120,
+            margin: EdgeInsets.only(left: 30, right: 30, bottom: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: Container(
+              padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: 'Chinese Side'),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                          5,
+                          (index) => Icon(
+                            Icons.star,
+                            color: AppColors.mainColor,
+                            size: 15,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      SmallText(text: '4.5'),
+                      SizedBox(width: 10),
+                      SmallText(text: '1287'),
+                      SizedBox(width: 10),
+                      SmallText(text: 'comments'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
