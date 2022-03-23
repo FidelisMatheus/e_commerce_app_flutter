2cfcd9cc803bec35acc8f19d6b9558c4b0b726b3
Matheus Fidelis <matheusfidelis@yahoo.com>
2022-03-23 14:51:42 -0300

    Stylization of HomePage complete
    First modifys in home page has been completed

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -70,7 +70,11 @@ class _MainFoodPageState extends State<MainFoodPage> {
             ),
           ),
           //showing the body
-          FoodPageBody(),
+          Expanded(
+            child: SingleChildScrollView(
+              child: FoodPageBody(),
+            ),
+          ),
         ],
       ),
     );
