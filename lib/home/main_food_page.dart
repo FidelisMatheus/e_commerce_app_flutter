39e090b2351d160b82891bf228aa835806acc257
Matheus Fidelis <matheusfidelis@yahoo.com>
2022-03-23 10:52:20 -0300

    Dynamic dimensions has created
    I finished first edits about dimensions and the 2 and half hours of video.

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce_app/home/food_page_body.dart';
 import 'package:e_commerce_app/utils/colors.dart';
+import 'package:e_commerce_app/utils/dimensions.dart';
 import 'package:e_commerce_app/widgets/big_text.dart';
 import 'package:e_commerce_app/widgets/small_text.dart';
 import 'package:flutter/material.dart';
@@ -20,8 +21,14 @@ class _MainFoodPageState extends State<MainFoodPage> {
           //showing the header
           Container(
             child: Container(
-              margin: EdgeInsets.only(top: 45, bottom: 15),
-              padding: EdgeInsets.only(left: 20, right: 20),
+              margin: EdgeInsets.only(
+                top: Dimensions.height45,
+                bottom: Dimensions.height15,
+              ),
+              padding: EdgeInsets.only(
+                left: Dimensions.width20,
+                right: Dimensions.width20,
+              ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -44,14 +51,16 @@ class _MainFoodPageState extends State<MainFoodPage> {
                   ),
                   Center(
                     child: Container(
-                      width: 45,
-                      height: 45,
+                      width: Dimensions.height45,
+                      height: Dimensions.height45,
                       child: Icon(
                         Icons.search,
                         color: Colors.white,
+                        size: Dimensions.iconSize24,
                       ),
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
+                        borderRadius:
+                            BorderRadius.circular(Dimensions.radius15),
                         color: AppColors.mainColor,
                       ),
                     ),
