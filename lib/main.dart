586208b95580ebf48cff399de4f2a77bc6aac22b
Matheus Fidelis <matheusfidelis@yahoo.com>
2022-03-28 10:47:58 -0300

    Recommended Food Detail Complete
    I completed food detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce_app/pages/food/popular_food_detail.dart';
+import 'package:e_commerce_app/pages/food/recommended_food_detail.dart';
 import 'package:e_commerce_app/pages/home/main_food_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: PopularFoodDetail(),
+      home: RecommendedFoodDetail(),
     );
   }
 }
