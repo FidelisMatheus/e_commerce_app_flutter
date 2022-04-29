d5bff6bafdf214c199b017c98e7268c5c92579e4
Matheus Fidelis <matheusfidelis@yahoo.com>
2022-04-29 09:36:27 -0300

    Add method for cart sharedPreferences

--- lib/data/repository/cart_repo.dart
+++ lib/data/repository/cart_repo.dart
@@ -1,1 +1,26 @@
-class CartRepo {}
+import 'dart:convert';
+
+import 'package:e_commerce_app/utils/app_constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../models/cart_model.dart';
+
+class CartRepo {
+  final SharedPreferences sharedPreferences;
+
+  CartRepo({required this.sharedPreferences});
+
+  List<String> cart = [];
+  void addToCartList(List<CartModel> cartList) {
+    cart = [];
+    /**
+     * converts objects to string because sharedpreferences only accepts strings.
+     */
+    cartList.forEach((element) {
+      return cart.add(jsonEncode(element));
+    });
+
+    sharedPreferences.setStringList(AppConstants.CART_LIST, cart);
+    print(sharedPreferences.getStringList(AppConstants.CART_LIST));
+  }
+}
--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -4,19 +4,24 @@ import 'package:e_commerce_app/data/api/api_client.dart';
 import 'package:e_commerce_app/data/repository/cart_repo.dart';
 import 'package:e_commerce_app/utils/app_constants.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../controllers/recommended_product_controller.dart';
 import '../data/repository/popular_product_repo.dart';
 import '../data/repository/recommended_product_repo.dart';
 
 Future<void> init() async {
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  Get.lazyPut(() => sharedPreferences);
+
   //api client
   Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL));
 
   //repos
   Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
   Get.lazyPut(() => RecommendedProductRepo(apiClient: Get.find()));
-  Get.lazyPut(() => CartRepo());
+  Get.lazyPut(() => CartRepo(sharedPreferences: Get.find()));
 
   //controllers
   Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
